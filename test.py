a28158f78ce8ead3082968d5609214f43b21873a
unknown <enzinigo@gmail.com>
2019-08-02 11:48:49 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  2 11:36:57 2019
+
+@author: arriba
+"""
+
+for num in range(6):
+    for i in range(num):
+        print('*', end='')
+    print('\n')
